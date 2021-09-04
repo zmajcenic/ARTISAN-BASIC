1ea2273559ab7670cdfe3beaa53dfc74ddd3efc5
Zoran Majcenić <68393992+zmajcenic@users.noreply.github.com>
2021-09-05 00:27:06 +0200

    MEMCPY

--- asm/main.asm
+++ asm/main.asm
@@ -1,21 +1,219 @@
  ORG 04000h
 
 ; INCLUDE "system_variables.inc"
-; INCLUDE "system_hooks.inc"
+ INCLUDE "system_hooks.inc"
 ; INCLUDE "bios_calls.inc"
 
 CHPUT   EQU     #A2
-CALBAS	EQU	#159
+CALBAS	EQU		#159
 ERRHAND EQU     #406F
 FRMEVL  EQU     #4C64
-FRESTR	EQU	#67D0
+FRESTR	EQU		#67D0
+; FRMQNT = formula quantificator
+; input HL=pointer to current program expression
+; output HL=next address
+; output DE=integer datum
+FRMQNT	EQU		#542F
+; GETBYT = get byte parameter
+; input HL=pointer to current program expression
+; output HL=next address
+; output A=E=byte read
+GETBYT	EQU		#521C
 CHRGTR  EQU     #4666
+SYNCHR	EQU		#558C
 VALTYP  EQU     #F663
 USR     EQU     #F7F8
-PROCNM	EQU	#FD89
+PROCNM	EQU		#FD89
+
+RAMAD0	EQU	0F341h	; Main-RAM Slot (00000h~03FFFh)
+RAMAD1	EQU	0F342h	; Main-RAM Slot (04000h~07FFFh)
+RAMAD2	EQU	0F343h	; Main-RAM Slot (08000h~0BFFFh)
+RAMAD3	EQU	0F344h	; Main-RAM Slot (0C000h~0FFFFh)
+EXPTBL	EQU #FCC1
 
  ; simulate cartridge with BASIC extension
  DW 04241H, 0, CALLHAND, 0, 0, 0, 0, 0
+
+; ****************************************************************************************************
+; function gets slot and subslot data for specific page
+; input A=page (0, 1 or 2)
+; output B = 0A8H register value
+; output D = 0 is no subslots, 1 if yes
+; output C = 0A8H value when page 3 slot equals to requested page slot
+; output E = subslot value if present
+; modifies AF, BC, DE, HL
+GET_PAGE_INFO:
+    LD L, A
+    ADD A, low (EXPTBL)
+    LD (GET_PAGE_INFO_L1+1), A
+    IN A, (0A8H)
+    LD B, A
+    AND 03FH
+    LD C, A
+GET_PAGE_INFO_L1:
+    LD A, (EXPTBL) ; modified by code above
+    AND 080H
+    JR Z, GET_PAGE_INFO_L2
+    ; expanded
+    DEC L
+    JP M, GET_PAGE_INFO_L3
+    DEC L
+    JP M, GET_PAGE_INFO_L4
+    ; page 2
+    RLCA
+    RLCA
+GET_PAGE_INFO_L5:
+    AND 0C0H
+    OR C
+    OUT (0A8H), A ; slot 3 = slot of page requested
+    LD C, A
+    LD A, (0FFFFH)
+    CPL
+    LD E, A
+    LD D, 1
+    LD A, B ; return stack
+    OUT (0A8H), A
+    RET 
+GET_PAGE_INFO_L2:
+    ; not expanded
+    LD D, 0
+    RET 
+GET_PAGE_INFO_L4:
+    ; page 1
+    RRCA
+    RRCA
+GET_PAGE_INFO_L3:
+    ; page 0
+    RRCA
+    RRCA
+    JR GET_PAGE_INFO_L5
+; ****************************************************************************************************
+
+; ****************************************************************************************************
+; function returns original slot and subslot info
+; input B = 0A8H register value
+; input D = 0 is no subslots, 1 if yes
+; input C = 0A8H value when page 3 slot equals to requested page slot
+; input E = subslot value if present
+; modifies AF
+RESTORE_PAGE_INFO:
+    LD A, D
+    OR A
+    JR Z, RESTORE_PAGE_INFO_L1
+    LD A, C
+    OUT (0A8H), A
+    LD A, E
+    LD (0FFFFH), A
+RESTORE_PAGE_INFO_L1:
+    LD A, B
+    OUT (0A8H), A
+    RET 
+; ****************************************************************************************************
+
+; *******************************************************************************************************
+; SELECTS A SLOT IN THE PAGE SPECIFIED BY AN ADDRESS.
+; INPUT:  A = SLOT ID: EXXXSSPP
+; E = EXPANDED FLAG
+; SS = SECONDARY SLOT NUMBER (ONLY IF EXPANDED)
+; PP = PRIMARY SLOT NUMBER
+;     HL = ADDRESS INSIDE THE PAGE TO CHANGE
+; CHANGES: AF, BC, DE
+
+LOCAL_ENASLT:
+    CALL L0353 
+    JP M, L0340 
+    IN A, (0A8H) 
+    AND C 
+    OR B 
+    OUT (0A8H), A 
+    RET 
+L0340:
+    PUSH HL 
+    CALL L0378 
+    LD C, A 
+    LD B, 0 
+    LD A, L 
+    AND H 
+    OR D 
+    LD HL, 0FCC5H 
+    ADD HL, BC 
+    LD (HL), A 
+    POP HL 
+    LD A, C 
+    JR LOCAL_ENASLT
+L0353:
+    DI 
+    PUSH AF 
+    LD A, H 
+    RLCA 
+    RLCA 
+    AND 3 
+    LD E, A 
+    LD A, 0C0H 
+L035D:
+    RLCA 
+    RLCA 
+    DEC E 
+    JP P, L035D
+    LD E, A 
+    CPL 
+    LD C, A 
+    POP AF 
+    PUSH AF 
+    AND 3 
+    INC A 
+    LD B, A 
+    LD A, 0ABH 
+L036E:
+    ADD A, 055H 
+    DJNZ L036E
+    LD D, A 
+    AND E 
+    LD B, A 
+    POP AF 
+    AND A 
+    RET
+L0378:
+    PUSH AF
+    LD A, D 
+    AND 0C0H 
+    LD C, A 
+    POP AF 
+    PUSH AF 
+    LD D, A 
+    IN A, (0A8H) 
+    LD B, A 
+    AND 03FH 
+    OR C 
+    OUT (0A8H), A 
+    LD A, D 
+    RRCA 
+    RRCA 
+    AND 3 
+    LD D, A 
+    LD A, 0ABH 
+L0390:
+    ADD A, 055H 
+    DEC D 
+    JP P, L0390
+    AND E 
+    LD D, A 
+    LD A, E 
+    CPL 
+    LD H, A 
+    LD A, (0FFFFH) 
+    CPL 
+    LD L, A 
+    AND H 
+    OR D 
+    LD (0FFFFH), A 
+    LD A, B 
+    OUT (0A8H), A 
+    POP AF 
+    AND 3 
+    RET
+; *******************************************************************************************************
+
 
 ; General BASIC CALL-instruction handler
  
@@ -50,7 +248,7 @@ CALLHAND:
 	CP	(HL)
 	JR	NZ,.CHKCMD	; Not end of table, go checking
 	POP	HL
-        SCF
+    SCF
 	RET
  
 .CALLDE:
@@ -63,15 +261,18 @@ CMDS:
 ; List of available instructions (as ASCIIZ) and execute address (as word)
  
 	DEFB	"UPRINT",0      ; Print upper case string
-	DEFW	_UPRINT
+	DEFW	UPRINT
  
 	DEFB	"LPRINT",0      ; Print lower case string
-	DEFW	_LPRINT
+	DEFW	LPRINT
+
+	DB "MEMCPY", 0
+	DW	MEMCPY
  
 	DEFB	0               ; No more instructions
  
 ;---------------------------
-_UPRINT:
+UPRINT:
 	CALL	EVALTXTPARAM	; Evaluate text parameter
 	PUSH	HL
         CALL    GETSTRPNT
@@ -94,7 +295,7 @@ _UPRINT:
         AND     %11011111
         RET
 ;---------------------------
-_LPRINT:
+LPRINT:
 	CALL	EVALTXTPARAM	; Evaluate text parameter
 	PUSH	HL
         CALL    GETSTRPNT
@@ -123,7 +324,7 @@ GETSTRPNT:
 ; HL = String Address
 ; B  = Lenght
  
-        LD      HL,(#F7F8)
+        LD      HL,(USR)
         LD      B,(HL)
         INC     HL
         LD      E,(HL)
@@ -175,4 +376,96 @@ SYNTAX_ERROR:
  
 ;---------------------------
  
+; *******************************************************************************************************
+; function to handle CALL MEMCPY basic extension
+; _MEMCPY ( INT source, 
+;			INT destination, 
+;			INT count, 
+;			(option) BYTE enable_ram,
+;			(option) BYTE wait_vsync)
+; enable_ram will put ram in page 0 also, page 1 is already there
+; wait_vsync will issue HALT before copying
+MEMCPY:
+	; opening (
+	CALL CHKCHAR
+	DB '('
+	; get source address
+	LD IX, FRMQNT
+	CALL CALBAS
+	PUSH DE
+	; comma
+	CALL CHKCHAR
+	DB ','
+	; get destination address
+	LD IX, FRMQNT
+	CALL CALBAS
+	PUSH DE
+	; comma
+	CALL CHKCHAR
+	DB ','
+	; get length
+	LD IX, FRMQNT
+	CALL CALBAS
+	PUSH DE
+	; comma
+	CALL CHKCHAR
+	DB ','
+	; get ROM/RAM
+	LD IX, GETBYT
+	CALL CALBAS
+	PUSH AF
+	; comma
+	CALL CHKCHAR
+	DB ','
+	; get vsync wait
+	LD IX, GETBYT
+	CALL CALBAS
+	PUSH AF
+	; ending )
+	CALL CHKCHAR
+	DB ')'
 
+	; save position
+	PUSH HL
+	POP IX
+
+	; syntax ok
+	; wait for vsync if needed
+	POP AF
+	OR A
+	JR Z, .L1
+	EI
+	HALT
+
+.L1:
+	; enable RAM in page 0 if needed
+	POP AF
+	OR A
+	; pop LDIR parameters and store away for later
+	POP BC
+	POP DE
+	POP HL
+	JR Z, .L2
+	EXX
+    XOR A
+    CALL GET_PAGE_INFO
+    PUSH BC
+    PUSH DE
+    LD A, (RAMAD0)
+    LD H, 0
+	DI
+    CALL LOCAL_ENASLT
+	EXX
+	LDIR
+    POP DE
+    POP BC
+    CALL RESTORE_PAGE_INFO
+	JR .L3
+
+.L2:
+	LDIR
+
+.L3:
+	PUSH IX
+	POP HL
+	RET
