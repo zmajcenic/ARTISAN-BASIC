daaa3d7d0e4e30c1d9a45b72061a724f7226dcc2
Zoran Majcenić <68393992+zmajcenic@users.noreply.github.com>
2021-09-25 16:16:01 +0200

    X

--- asm/main.asm
+++ asm/main.asm
@@ -194,6 +194,10 @@ CMDS_S:
 CMDS_B:
 	DB "BLIT", 0
 	DW BLIT
+	DB "BOXMEMCPY", 0
+	DW BOXMEMCPY
+	DB "BOXMEMVRM", 0
+	DW BOXMEMVRM
 	DB 0
 CMDS_T:
 	DB "TILERAM", 0
@@ -2097,7 +2101,7 @@ TILE:
 
 ; *******************************************************************************************************
 ; function to handle CALL TILERAM basic extension
-; rotates 1-bit character drawing horizontally with mask and character data and
+; fills memory with tiles
 ; fuses with background data and applies vertical shift too
 ; TILERAM ( INT request_data_ptr )
 ; request_data_ptr described in TILE
@@ -2145,7 +2149,7 @@ TILERAM:
 
 ; *******************************************************************************************************
 ; function to handle CALL TILEVRM basic extension
-; rotates 1-bit character drawing horizontally with mask and character data and
+; fills vram with tiles
 ; fuses with background data and applies vertical shift too
 ; TILEVRM ( INT request_data_ptr )
 ; request_data_ptr described in TILE
@@ -2191,5 +2195,140 @@ TILEVRM:
 	JP SETWRT_LOCAL
 ; *******************************************************************************************************
 
+; *******************************************************************************************************
+; generic function to implement rectangle data copy
+; should be modified to call appropriate function for memory or vram
+; input IX=pointer to following structure
+; +00 source data pointer
+; +02 num bytes in a row
+; +04 number of rows
+; +06 source add-to value till next row
+; +08 destination address
+; +10 destination add-to value till next row
+; modifies AF, BC, DE, HL
+RECTANGLE_COPY:
+	LD L, (IX+0)
+	LD H, (IX+1) ; source address
+	LD E, (IX+8)
+	LD D, (IX+9) ; destination
+	LD B, (IX+4) ; row number
+.L1:
+	PUSH BC
+		PUSH HL
+			PUSH DE
+				LD C, (IX+2)
+				LD B, (IX+3) ; num bytes in a row
+.CALL1:
+				CALL 0 ; set destination address from DE
+.CALL2:
+				CALL 0 ; copy data fn
+			POP HL
+			LD C, (IX+10)
+			LD B, (IX+11) ; destination add-to
+			ADD HL, BC
+			EX DE, HL
+		POP HL
+		LD C, (IX+6)
+		LD B, (IX+7) ; src add-to
+		ADD HL, BC
+	POP BC
+	DJNZ .L1
+	RET
+; *******************************************************************************************************
+
+; *******************************************************************************************************
+; function to handle CALL BOXMEMCPY basic extension
+; copies data with window like boundaries to ram
+; BOXMEMCPY ( INT request_data_ptr )
+; request_data_ptr described in RECTANGLE_COPY
+; will put ram in page 0 also, page 1 is already there
+BOXMEMCPY:
+	; opening (
+	CALL CHKCHAR
+	DB '('
+	; get pointer to request struct
+	LD IX, FRMQNT
+	CALL CALBAS
+	PUSH DE
+	; ending )
+	CALL CHKCHAR
+	DB ')'
+
+	POP IX ; pointer to request struct
+
+	PUSH HL ; save position in BASIC buffer
+
+	LD IY, .RET
+	JP ENABLE_PAGE0
+.RET:
+	EI
+	; set RAM functions to call
+	LD HL, 0
+	LD (RECTANGLE_COPY.CALL1), HL ; NOP NOP
+	LD (RECTANGLE_COPY.CALL1+2), HL ; NOP NOP
+	LD HL, #B0ED ; LDIR
+	LD (RECTANGLE_COPY.CALL1+4), HL
+	CALL RECTANGLE_COPY
+
+    POP DE
+    POP BC
+    CALL RESTORE_PAGE_INFO
+
+	POP HL
+	RET
+; *******************************************************************************************************
+
+; *******************************************************************************************************
+; function to handle CALL BOXMEMVRM basic extension
+; copies data with window like boundaries to ram
+; BOXMEMVRM ( INT request_data_ptr )
+; request_data_ptr described in RECTANGLE_COPY
+; will put ram in page 0 also, page 1 is already there
+BOXMEMVRM:
+	; opening (
+	CALL CHKCHAR
+	DB '('
+	; get pointer to request struct
+	LD IX, FRMQNT
+	CALL CALBAS
+	PUSH DE
+	; ending )
+	CALL CHKCHAR
+	DB ')'
+
+	POP IX ; pointer to request struct
+
+	PUSH HL ; save position in BASIC buffer
+
+	LD IY, .RET
+	JP ENABLE_PAGE0
+.RET:
+	EI
+	; set RAM functions to call
+	LD HL, .SETDEST
+	LD (RECTANGLE_COPY.CALL1+1), HL
+	LD HL, .COPYDATA
+	LD (RECTANGLE_COPY.CALL2+1), HL
+	LD A, #CD ; CALL
+	LD (RECTANGLE_COPY.CALL1), A
+	LD (RECTANGLE_COPY.CALL2), A
+	CALL RECTANGLE_COPY
+
+    POP DE
+    POP BC
+    CALL RESTORE_PAGE_INFO
+
+	POP HL
+	RET
+.SETDEST:
+	EX DE, HL
+	CALL SETWRT_LOCAL
+	EX DE, HL
+	RET	
+.COPYDATA:
+	LD B, C
+	LD C, #98
+	JP MEMVRM.BBYTECOPY
+; *******************************************************************************************************
 
 EXT_END:
