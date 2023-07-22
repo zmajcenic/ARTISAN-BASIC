f9c9cd5230ba394169057f9692a4aba0600fd9e5
Zoran Majcenić <68393992+zmajcenic@users.noreply.github.com>
2023-07-22 20:02:21 +0200

    x

--- append_ctrl_z.sh
+++ append_ctrl_z.sh
@@ -1,2 +1,3 @@
-for a in dsk/*.ASC; do echo -n $'\032'>>$a; done
-for a in dsk/*.BAS; do echo -n $'\032'>>$a; done
+for a in dsk/*.ASC;do echo $a;echo -n $'\032'>>$a;done
+for a in dsk/*.BAS;do echo $a;echo -n $'\032'>>$a;done
+
